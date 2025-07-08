9b093ff8c52e0ed219505ff4de8f104ab662201a
unknown <al.developer12@gmail.com>
2025-07-08 18:11:15 +0700

    update model

--- models/todo_group.go
+++ models/todo_group.go
@@ -2,9 +2,9 @@ package models
 
 type TodoGroup struct {
 	BaseModel
-	Name        string            `gorm:"type:text;not null" json:"name" validate:"required"`
-	Description string            `gorm:"type:text" json:"description"`
-	
-	Tasks       []Task            `gorm:"foreignKey:TodoGroupID" json:"tasks"`
-	Members     []TodoGroupMember `gorm:"foreignKey:TodoGroupID" json:"members"`
+	Name        string `gorm:"type:text;not null" json:"name" validate:"required"`
+	Description string `gorm:"type:text" json:"description"`
+
+	Tasks   []Task            `gorm:"foreignKey:TodoGroupID" json:"tasks"`
+	Members []TodoGroupMember `gorm:"foreignKey:TodoGroupID;references:ID" json:"members"`
 }
