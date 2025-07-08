4d878d66f5eb1f7e49400cbf20ffddef6171faf5
unknown <al.developer12@gmail.com>
2025-07-08 18:18:29 +0700

    update model

--- models/todo_group_member.go
+++ models/todo_group_member.go
@@ -8,10 +8,10 @@ import (
 
 type TodoGroupMember struct {
 	BaseModel
-	TodoGroupID uuid.UUID  `gorm:"type:uuid;not null;index" json:"todo_group_id" validate:"required"`
-	UserID      uuid.UUID  `gorm:"type:uuid;not null;index" json:"user_id" validate:"required"`
-	JoinedAt    time.Time  `gorm:"autoCreateTime" json:"joined_at"`
-	
-	TodoGroup   *TodoGroup  `gorm:"foreignKey:TodoGroupID" json:"todo_group"`
-	User        *User       `gorm:"foreignKey:UserID" json:"user"`
+	TodoGroupID uuid.UUID `gorm:"type:uuid;not null;index" json:"todo_group_id" validate:"required"`
+	UserID      uuid.UUID `gorm:"type:uuid;not null;index" json:"user_id" validate:"required"`
+	JoinedAt    time.Time `gorm:"autoCreateTime" json:"joined_at"`
+
+	TodoGroup *TodoGroup `gorm:"foreignKey:TodoGroupID;references:ID" json:"todo_group"`
+	User      *User      `gorm:"foreignKey:UserID;references:ID" json:"user"`
 }
