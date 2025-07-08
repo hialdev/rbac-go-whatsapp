b8fb0549df0c9742da74cb638803c120093b2872
unknown <al.developer12@gmail.com>
2025-07-08 15:44:58 +0700

    update models

--- models/notification.go
+++ models/notification.go
@@ -4,13 +4,15 @@ import "github.com/google/uuid"
 
 type Notification struct {
 	BaseModel
-	Title       string    `gorm:"type:text;not null" json:"title"`
-	Description string    `gorm:"type:text" json:"description"`
-	Issue       string    `gorm:"type:text;not null" json:"issue" validate:"oneof=discussion assign done"`
-	TodoGroupID uuid.UUID `gorm:"type:uuid;index" json:"todo_group_id"`
+	Title       string     `gorm:"type:text;not null" json:"title"`
+	Description string     `gorm:"type:text" json:"description"`
+	Issue       string     `gorm:"type:text;not null" json:"issue" validate:"oneof=discussion assign done"`
+	TodoGroupID uuid.UUID  `gorm:"type:uuid;index" json:"todo_group_id"`
+	UserID      uuid.UUID  `gorm:"type:uuid;index" json:"user_id"`
 	TaskID      *uuid.UUID `gorm:"type:uuid;index" json:"task_id,omitempty"`
-	IsRead      bool      `gorm:"default:false" json:"is_read"`
+	IsRead      bool       `gorm:"default:false" json:"is_read"`
 
 	TodoGroup *TodoGroup `gorm:"foreignKey:TodoGroupID" json:"todo_group,omitempty"`
 	Task      *Task      `gorm:"foreignKey:TaskID" json:"task,omitempty"`
+	User      *User      `gorm:"foreignKey:UserID" json:"user,omitempty"`
 }
